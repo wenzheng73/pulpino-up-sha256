9d0440f39d72c7b0e7c348baf06f551e19c3840f
1997zzz <2419459837@qq.com>
2021-10-21 11:28:23 +0800

    change xilinx_2020_3.sh

--- fpga/xilinx_2020_3.sh
+++ fpga/xilinx_2020_3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export XILINX_HOME=/home/wanjun/vivado/2020.3
+#export XILINX_HOME=/home/wanjun/vivado/2020.3
 if [ ! -n "$XILINX_HOME" ]; then
 	echo [ERROR]:XILINX_HOME does not exist, please set. etc:export XILINX_HOME="your vivado install location"
 	exit 1
