0754ac79a0db6ce2b7911ffc5f27d73fabcd6462
1997zzz <2419459837@qq.com>
2021-10-22 15:05:41 +0800

    change vivado's version

--- fpga/common/messages.tcl
+++ fpga/common/messages.tcl
@@ -1,5 +1,4 @@
 # sets up Vivado messages in a more sensible way
-
 set_msg_config -id {[Synth 8-3352]}         -new_severity "critical warning"
 set_msg_config -id {[Synth 8-350]}          -new_severity "critical warning"
 set_msg_config -id {[Synth 8-2490]}         -new_severity "warning"
@@ -11,4 +10,6 @@ set_msg_config -id {[Opt 31-35]}            -new_severity "info"
 set_msg_config -id {[Opt 31-32]}            -new_severity "info"
 set_msg_config -id {[Shape Builder 18-119]} -new_severity "warning"
 set_msg_config -id {[Filemgmt 20-742]}      -new_severity "error"
+
+
 
